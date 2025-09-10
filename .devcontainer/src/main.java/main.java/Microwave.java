b7345dceb07659fe3227ab9e1e4702d4c3cf5750
syau28-glitch <syau28@bfacademy.de>
2025-09-10 13:05:42 +0000

    Added Microwave.java

---
+++ .devcontainer/src/main.java/main.java/Microwave.java
@@ -0,0 +1,57 @@
+public class Microwave
+{
+    //attributes
+    private String brand;
+    private int maxPower;
+
+    public Microwave (String manufacturer, int power)
+    {
+        brand = manufacturer;
+        maxPower = power;
+    }
+
+    //microwave behaviours
+    public void spin()
+    {
+        System.out.println("Spinning");
+    }
+    public void spin(boolean spinVal) //same name is okay with different parameters
+    {
+        if (spinVal) 
+        {
+            System.out.println("Spinning");
+        }
+    }
+    public void cook()
+    {
+        System.out.println("Cooking at " + maxPower + " W");
+        spin();
+        beep();
+    }
+    public void cook(int seconds, int power)
+    {
+        System.out.println("Cooking for " + seconds + " seconds at " + power + " W");
+        spin(true);
+        beep();
+    }
+    public void cook30()
+    {
+        cook(30, maxPower);
+    }
+    public void beep()
+    {
+        System.out.println("Beep!");
+    }
+    public void defrost(int time, int power)
+    {
+
+    }
+    public static void main(String[] args)
+    {
+        Microwave superCoolMicrowave = new Microwave("Samsung", 800);
+        System.out.println("Your microwave was made by " + superCoolMicrowave.brand);
+        superCoolMicrowave.cook30();
+        superCoolMicrowave.cook(3000, 700);
+        superCoolMicrowave.cook();
+    }
+}
