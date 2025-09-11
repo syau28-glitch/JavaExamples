4968233f8cf1deb41ea566c1e1f3b09dcf878cb3
syau28-glitch <syau28@bfacademy.de>
2025-09-11 10:31:45 +0000

    Edited Main.java and created RandomStuff.java

--- .devcontainer/src/main.java/main.java/Main.java
+++ .devcontainer/src/main.java/main.java/Main.java
@@ -2,15 +2,17 @@ public class Main
 {
     public static void main(String[] args)
     {
-        //Microwave bob = new Microwave("BFA", 1000);
-        //bob.cook30();
-        LanguageHelper.greet();
+        Microwave bob = new Microwave("BFA", 1000);
+        bob.cook30();
+        /*LanguageHelper.greet();
         if (LanguageHelper.happy()) {
             System.out.println("happy");
         }else {
             System.out.println("sad");
         }
-        System.out.println(LanguageHelper.salutation());
-        System
+        System.out.println(LanguageHelper.salutation());*/
+        Microwave fred = new Microwave("BFA", 1000);
+        System.out.print("bob: " + bob);
+        System.out.println(", fred: " +fred);
     }
 }
